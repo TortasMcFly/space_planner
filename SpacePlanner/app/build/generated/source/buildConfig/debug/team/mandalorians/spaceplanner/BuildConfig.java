698db6295531567cab671570517a2bd5ea14d5c9
Alejandro García Olivo <ago99@live.com.mx>
2020-08-29 15:40:52 -0500

    onBoarding
    ~ onBoarding Page

---
+++ SpacePlanner/app/build/generated/source/buildConfig/debug/team/mandalorians/spaceplanner/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package team.mandalorians.spaceplanner;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "team.mandalorians.spaceplanner";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
